23dfdb46fc6c7bbd52eced5006b6238c0c2ff219
Agoston <dagoston93@gmail.com>
2021-02-08 10:40:10 +0100

    xd

--- pong.asm
+++ pong.asm
@@ -1,6 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; TODO:
 ;;; ADDING THIS LINE TO TEST GITHUB
+
 ;;;   -- change assembler to NESASM                                         -- DONE
 ;;;   -- jump table for gamestates                                          -- DONE
 ;;;   -- separate parts to files                                            -- DONE
